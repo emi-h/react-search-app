039fd84ff98bfcdd32506431276a1b4570324809
emih <emi.programming@gmail.com>
2022-06-11 23:01:20 +0900

    ✨ Add Search feature

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,16 @@ import { useState, useEffect, useRef } from 'react'
 
 function App() {
   const [users, setUsers] = useState(null);
+  const [searchQuery, setSearchQuery] = useState();
   const inputEl = useRef(null);
 
   const handleInput = () => {
-    console.log(inputEl.current.value);
+    setSearchQuery(
+      // filter
+      users.filter((user) =>
+        user.name.toLowerCase().includes(inputEl.current.value)
+      )
+    );
   }
 
   async function fetchUserData() {
@@ -17,17 +23,13 @@ function App() {
     fetchUserData();
   }, []);
 
-  if (!users) {
-    return "loading...";
-  }
-
   return (
     <div className="App">
       <div className='main'>
         <h2>Search App</h2>
         <input ref={inputEl} type="text" onChange={handleInput} />
         <div className='content'>
-          {users ? users.map((user) =>
+          {searchQuery ? searchQuery.map((user) =>
             <div className='box' key={user.id}>
               <h3>{user.name}</h3>
               <hr />
